0cbee29fd06603ea6ec82e3bbe4cc693e5f29480
kinestetika <kinestetika@gmail.com>
2022-04-08 20:44:51 -0600

    added package files

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='metaerg',
+    version='2.0',
+    packages=setuptools.find_packages(where='src'),
+    url='https://github.com/kinestetika/MetaErg',
+    license='MIT',
+    author='Marc Strous',
+    author_email='mstrous@ucalgary.ca',
+    description='Annotation of genomes and contigs',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.10',
+                 'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    keywords='repeat-regions genes functions taxonomy',
+    project_urls={'Source': 'https://github.com/kinestetika/MetaErg'},
+    package_dir={'': 'src'},
+    python_requires='>=3.10',
+    install_requires=['tqdm', 'biopython'],
+    extras_require={  # Optional
+        'dev': ['setuptools', 'build', 'twine'],
+        'test': [],
+    },
+    entry_points={  # Optional
+        'console_scripts': [
+            'calisp=calisp.main:main',
+        ],
+    }
+)
